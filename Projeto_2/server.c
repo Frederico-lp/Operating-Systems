77e91d35b8e1decf331fed7f742654da1b57d5f4
Frederico Lopes <frederico.m.lps@gmail.com>
2021-05-02 15:23:22 +0100

    new file

---
+++ Projeto_2/server.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+
+#include <dirent.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+
+#include <signal.h>
+#include <pthread.h>
