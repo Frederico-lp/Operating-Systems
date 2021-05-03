34eed46bb9716c6ba7e0859a78443cbef54b3e83
Frederico Lopes <frederico.m.lps@gmail.com>
2021-05-03 14:57:09 +0100

    main developement

--- Projeto_2/server.c
+++ Projeto_2/server.c
@@ -15,6 +15,10 @@
 
 #include <signal.h>
 #include <pthread.h>
+#include <semaphore.h>
+
+sem_t *bufszSem;
+static int public_pipe;
 
 typedef struct Messages { //é diferente não?
     int rid;    // request id
@@ -23,6 +27,17 @@ typedef struct Messages { //é diferente não?
     int tskload;    // task load
     int tskres;    // task result
 } Message;
+
+Message* create_msg() {
+    Message* msg = (Message*)malloc(sizeof(Message));
+    msg->rid = requestNumber;
+    msg->tskload = rand() % 9 + 1;
+    msg->pid = getpid();
+    msg->tid = pthread_self();
+    msg->tskres = -1;
+
+    return msg;
+}
 
 void op_print(char op[], Message msg) {
     time_t cur_time = time(NULL);
@@ -37,46 +52,76 @@ void op_print(char op[], Message msg) {
     fflush(stdout);
 }
 
+void* serverThread() {
+}
+
 int main(int argc, char* argv[], char* envp[]) {
-    int nsecs;
-    char fifoname[25], buffer[30];
+    int nsecs, bufsz = 0;
+    char publicFIFO[25], buffer[30];
     srand(time(NULL));
 
-    if (argc != 4) {
-        perror("Usage: s <-t nsecs> fifoname");
+    if (argc != 4 || argc != 6) {
+        perror("Usage: s <-t nsecs>  [-l bufsz] fifoname");
         exit(1);
     }
+
     nsecs = atoi(argv[2]);
-    strcpy(fifoname, argv[3]);  //se n tiver /tmp/ acrescentar
-    if (strstr(fifoname, "/tmp/") == NULL) {
-        sprintf(buffer, "/tmp/%s", fifoname);
-        strcpy(fifoname, buffer);
+
+    if(argc == 4)
+        strcpy(publicFIFO, argv[3]);  
+    else if (argc == 6){
+        strcpy(publicFIFO, argv[5]); 
+        bufsz = atoi(argv[4]);
     }
+
+    if (strstr(publicFIFO, "/tmp/") == NULL) {
+        sprintf(buffer, "/tmp/%s", publicFIFO);
+        strcpy(publicFIFO, buffer);
+    }
+
+
+    if(sem_init(bufszSem, 1, bufsz) == -1)    //n pode ser 0 para n ser partilhado, 1??
+        perror("Error creating semaphore\n");
+
+
     time_t start = time(NULL);
     time_t endwait = time(NULL) + nsecs;
 
-    for (int tries = 0; tries < 3; tries++) {
-        public_pipe = open(fifoname, O_WRONLY);
-        if (public_pipe != -1)
-            break;
-        sleep(1);
-    }
-    if (public_pipe == -1) {
-        perror("Error opening public FIFO");
+
+    if(mkfifo(publicFIFO, 0660) == -1){
+        perror("Error creating public FIFO");
         exit(1);
     }
+
+    if((public_pipe = open(publicFIFO, O_RDONLY)) == -1){
+        perror("Error opening public FIFO in server");
+        exit(1);
+    }
+        
     pthread_t tid;
     Message* msg;
     bool processing = true;
     int reading;
 
     while (start < endwait) {
-        msg = (Message*)malloc(sizeof(Message));
+        msg = create_msg();
         reading = read(public_pipe, msg, sizeof(Message));
 
         if (reading > 0) {
-            op_print(msg, RECVD);
-            //do stuff
+            op_print("RECVD", *msg);    //??
+
+            if(sem_wait(bufszSem) == -1){
+                free(msg);
+                perror("Error waiting for semaphore\n");
+                break;
+            }
+
+            if (pthread_create(&tid, NULL, serverThread, NULL)) {
+                perror("Error creating thread");
+                exit(1);
+        }
+
+
         }
         else if (reading == 0) {
             free(msg);
@@ -84,6 +129,7 @@ int main(int argc, char* argv[], char* envp[]) {
         else {
             perror("Error reading public FIFO");
             free(msg);
+            break;
         }
     }
 
